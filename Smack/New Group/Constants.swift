dbe6acaced422bb24e8f12b7a4b12287046aebbf
Admin <admin@Admins-MacBook-Pro.local>
2018-06-20 14:17:11 -0700

    added Login VC

---
+++ Smack/New Group/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Admin on 6/20/18.
+//  Copyright © 2018 Matthew Heinrichs. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Admin on 6/20/18.
+//  Copyright © 2018 Matthew Heinrichs. All rights reserved.
+//
+
+import Foundation
+
+//segues
+
+let TO_LOGIN = "toLogin"
